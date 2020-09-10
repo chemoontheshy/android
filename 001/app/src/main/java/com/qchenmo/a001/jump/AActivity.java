7c3beaa1490b02ae57ff4f3fa5a7ae879f3ecc23
chemoontheshy <xzfandzgx@gmail.com>
2020-09-10 14:48:29 +0800

    20200910

--- 001/app/src/main/java/com/qchenmo/a001/jump/AActivity.java
+++ 001/app/src/main/java/com/qchenmo/a001/jump/AActivity.java
@@ -1,10 +1,13 @@
 package com.qchenmo.a001.jump;
 
-import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.qchenmo.a001.R;
 
@@ -13,22 +16,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AActivity extends AppCompatActivity {
 
-    Button btn_toBactivity;
+    Button btn_toBActivity,btn_AtoA;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
 
-        btn_toBactivity = findViewById(R.id.btn_toBActivity);
-        btn_toBactivity.setOnClickListener(new View.OnClickListener() {
+        Log.d("AActivity","----------onCreate-----------");
+        Log.d("AActivity","taskId:"+getTaskId()+" ,hash:"+hashCode());
+        LogTaskName();
+        btn_toBActivity = findViewById(R.id.btn_toBActivity);
+        btn_AtoA = findViewById(R.id.btn_toAActivity);
+        btn_AtoA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AActivity.this,AActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_toBActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //显式跳转1
-                Intent intent = new Intent(AActivity.this,BActivity.class);
-                intent.putExtra("name","沉默");
-                intent.putExtra("id",001);
+                Intent intent = new Intent(AActivity.this, BActivity.class);
+                intent.putExtra("name", "沉默");
+                intent.putExtra("id", 001);
                 startActivity(intent);
+                //startActivityForResult(intent,0);
                 //显式跳转2
 //                Intent intent = new Intent();
 //                intent.setClass(AActivity.this,BActivity.class);
@@ -49,5 +64,28 @@ public class AActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(AActivity.this, data.getExtras().getString("title"), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("AActivity","----------onNewIntent-----------");
+        Log.d("AActivity","taskId:"+getTaskId()+" ,hash:"+hashCode());
+        LogTaskName();
+    }
+
+    private void LogTaskName(){
+        try {
+            ActivityInfo info = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+            Log.d("AActivity",info.taskAffinity);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- 001/app/src/main/java/com/qchenmo/a001/jump/BActivity.java
+++ 001/app/src/main/java/com/qchenmo/a001/jump/BActivity.java
@@ -1,6 +1,12 @@
 package com.qchenmo.a001.jump;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.qchenmo.a001.R;
@@ -11,15 +17,58 @@ import androidx.appcompat.app.AppCompatActivity;
 public class BActivity extends AppCompatActivity {
 
     private TextView mTvGet_A_data;
+
+    private Button mBtnFinish,btn_toAActivity;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+        Log.d("BActivity","----------onNewIntent-----------");
+        Log.d("BActivity","taskId:"+getTaskId()+" ,hash:"+hashCode());
+        LogTaskName();
         mTvGet_A_data = findViewById(R.id.get_A_data);
+        mBtnFinish = findViewById(R.id.btn_finish);
+        btn_toAActivity = findViewById(R.id.btn_BtoA);
+        btn_toAActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(BActivity.this,AActivity.class);
+                startActivity(intent);
+            }
+        });
         Bundle bundle = getIntent().getExtras();
         String name =  bundle.getString("name");
         int id = bundle.getInt("id");
         mTvGet_A_data.setText("name:"+name+" id:"+id);
 
+        mBtnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("title","我回来了");
+                setResult(AActivity.RESULT_OK,intent);
+                finish();
+
+            }
+        });
+
+
+
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("BActivity","----------onCreate-----------");
+        Log.d("BActivity","taskId:"+getTaskId()+" ,hash:"+hashCode());
+        LogTaskName();
+    }
+
+    private void LogTaskName(){
+        try {
+            ActivityInfo info = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+            Log.d("BActivity",info.taskAffinity);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
