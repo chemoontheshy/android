3ab3684785d9bb79db03479522d042bb87f3861c
chemoontheshy <xzfandzgx@gmail.com>
2020-09-16 19:27:21 +0800

    add new ff001

--- ff001/app/src/androidTest/java/com/qchenmo/ff001/ExampleInstrumentedTest.java
+++ ff001/app/src/androidTest/java/com/qchenmo/ff001/ExampleInstrumentedTest.java
@@ -1,9 +1,9 @@
-package com.qchenmo.nfc;
+package com.qchenmo.ff001;
 
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.qchenmo.nfc", appContext.getPackageName());
+        assertEquals("com.qchenmo.ff001", appContext.getPackageName());
     }
 }
