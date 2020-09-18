00e264122e520db7fb768d8a68e6f1f8c82404e5
chemoontheshy <xzfandzgx@gmail.com>
2020-09-18 18:55:45 +0800

    update

--- ff001/app/src/main/cpp/native-lib.cpp
+++ ff001/app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,290 @@
 #include <jni.h>
-#include <string>
+#include <android/log.h>
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
 
-extern "C" JNIEXPORT jstring
+extern "C" {
+#include "include/libavcodec/avcodec.h"
+#include "include/libavformat/avformat.h"
+#include "include/libswscale/swscale.h"
+#include "include/libavutil/time.h"
+#include "include/libavutil/mathematics.h"
+#include "include/libavutil/imgutils.h"
+#include "include/libavutil/pixfmt.h"
 
-JNICALL
-Java_com_qchenmo_ff001_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+}
+
+#define TAG "native" // 这个是自定义的LOG的标识
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__) // 定义LOGD类型
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__) // 定义LOGI类型
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__) // 定义LOGW类型
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__) // 定义LOGF类型
+
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_qchenmo_ff001_MainActivity_hello(JNIEnv *env, jobject thiz) {
+    // TODO: implement hello()
+    AVFormatContext *iFormatCtx;
+    AVCodecContext *iCodecCtx;
+    AVCodec *iCodec;
+    AVFrame *iFrame, *oFrameRGB;
+    AVPacket *packet;
+    uint8_t *out_buffer;
+    int videoStream = -1;
+    int i, numBytes;
+    int ret, got_picture;
+    static struct SwsContext *img_convert_ctx;
+
+    //新增
+    const char *error;
+    //ANativeWindow *nativeWindow = ANativeWindow_fromSurface(env, surface);
+    //ANativeWindow_Buffer windowBuffer;
+
+
+    //赋予结构体
+    iFormatCtx = avformat_alloc_context();
+    iCodecCtx = avcodec_alloc_context3(nullptr);
+    iFrame = av_frame_alloc();
+    oFrameRGB = av_frame_alloc();
+
+    //输入地址
+    const char *rtspURL;
+    rtspURL = "rtsp://192.168.1.103/11";
+
+    //FFmpeg 2.网络配置
+    avformat_network_init();
+    AVDictionary *avdic = nullptr;
+    av_dict_set(&avdic, "rtsp_transport", "tcp", 0);
+    av_dict_set(&avdic, "max_delay", "100", 0);
+
+    //新增
+    av_dict_set(&avdic, "buffer_size", "1024000", 0);
+    av_dict_set(&avdic, "setTimeOut", "3000000", 0);
+
+    //3.打开输入流
+    ret = avformat_open_input(&iFormatCtx, rtspURL, nullptr, &avdic);
+    if (ret < 0) {
+        return env->NewStringUTF("error");
+
+    }
+    //4.寻找输入码流
+    ret = avformat_find_stream_info(iFormatCtx, nullptr);
+    if (ret < 0) {
+        return env->NewStringUTF("Failed to retrieve input stream information.");
+    }
+
+    //5.循环出视频流
+
+    for (i = 0; i < iFormatCtx->nb_streams; i++) {
+        if (iFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoStream = i;
+            break;
+        }
+    }
+
+    //6.复制视频流打信息
+    avcodec_parameters_to_context(iCodecCtx, iFormatCtx->streams[videoStream]->codecpar);
+    iCodec = avcodec_find_decoder(iCodecCtx->codec_id);
+    if (iCodec == nullptr) {
+        return env->NewStringUTF("Codec not fount.");
+    }
+
+    //7.打开解码器
+    if (avcodec_open2(iCodecCtx, iCodec, nullptr) < 0) {
+        error = "Could not open codec,";
+        return env->NewStringUTF(error);
+    }
+
+    //8解码
+    img_convert_ctx = sws_getContext(iCodecCtx->width, iCodecCtx->height, iCodecCtx->pix_fmt,
+                                     iCodecCtx->width, iCodecCtx->height, AV_PIX_FMT_RGBA,
+                                     SWS_BICUBIC,
+                                     nullptr, nullptr, nullptr);
+    numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGBA, iCodecCtx->width, iCodecCtx->height, 1);
+    out_buffer = (uint8_t *) av_malloc(numBytes * sizeof(uint8_t));
+
+    av_image_fill_arrays(oFrameRGB->data, oFrameRGB->linesize, out_buffer, AV_PIX_FMT_RGBA,
+                         iCodecCtx->width, iCodecCtx->height, 1);
+    int y_size = iCodecCtx->width * iCodecCtx->height;
+    packet = (AVPacket *) malloc(sizeof(AVPacket));
+    av_new_packet(packet, y_size);
+
+    int num = 0;
+    while (1) {
+        if (av_read_frame(iFormatCtx, packet) < 0) {
+            break;
+        }
+        if (packet->stream_index == videoStream) {
+            ret = avcodec_send_packet(iCodecCtx, packet);
+            if (ret < 0) {
+                break;
+            }
+            got_picture = avcodec_receive_frame(iCodecCtx, iFrame);
+            if (!got_picture) {
+                //__android_log_print(ANDROID_LOG_ERROR, "native-log","w");
+                sws_scale(img_convert_ctx, (uint8_t const *const *) iFrame->data, iFrame->linesize,
+                          0, iCodecCtx->height, oFrameRGB->data, oFrameRGB->linesize);
+                num++;
+                LOGD("decode is %d\n frame",num);
+
+            }
+        }
+        av_packet_unref(packet);
+    }
+
+
+    av_free(out_buffer);
+    av_free(iFrame);
+    av_free(oFrameRGB);
+    avcodec_close(iCodecCtx);
+    avformat_close_input(&iFormatCtx);
+
+    return env->NewStringUTF("ok");
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_qchenmo_ff001_MainActivity_play(JNIEnv *env, jobject thiz, jstring url, jobject surface) {
+    // TODO: implement play()
+    AVFormatContext *iFormatCtx;
+    AVCodecContext *iCodecCtx;
+    AVCodec *iCodec;
+    AVFrame *iFrame, *oFrameRGB;
+    AVPacket *packet;
+    uint8_t *out_buffer;
+    int videoStream = -1;
+    int i, numBytes;
+    int ret, got_picture;
+    static struct SwsContext *img_convert_ctx;
+
+    //新增
+    const char *error;
+    ANativeWindow *nativeWindow = ANativeWindow_fromSurface(env, surface);
+    ANativeWindow_Buffer windowBuffer;
+
+
+    //赋予结构体
+    iFormatCtx = avformat_alloc_context();
+    iCodecCtx = avcodec_alloc_context3(nullptr);
+    iFrame = av_frame_alloc();
+    oFrameRGB = av_frame_alloc();
+
+    //输入地址
+    const char *rtspURL;
+    rtspURL = "rtsp://192.168.1.103/11";
+
+    //FFmpeg 2.网络配置
+    avformat_network_init();
+    AVDictionary *avdic = nullptr;
+    av_dict_set(&avdic, "rtsp_transport", "tcp", 0);
+    av_dict_set(&avdic, "max_delay", "100", 0);
+
+    //新增
+    av_dict_set(&avdic, "buffer_size", "1024000", 0);
+    av_dict_set(&avdic, "setTimeOut", "3000000", 0);
+
+    //3.打开输入流
+    ret = avformat_open_input(&iFormatCtx, rtspURL, nullptr, &avdic);
+    if (ret < 0) {
+        __android_log_print(ANDROID_LOG_INFO, "native-log", "Could not open the RTSP URL");
+        return;
+    }
+
+    //4.寻找输入码流
+    ret = avformat_find_stream_info(iFormatCtx, nullptr);
+    if (ret < 0) {
+         error= "Failed to retrieve input stream information.";
+         LOGD("error: %s\n",error);
+        return;
+    }
+
+    //5.循环出视频流
+
+    for (i = 0; i < iFormatCtx->nb_streams; i++) {
+        if (iFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoStream = i;
+            break;
+        }
+    }
+
+    //6.复制视频流打信息
+    avcodec_parameters_to_context(iCodecCtx, iFormatCtx->streams[videoStream]->codecpar);
+    iCodec = avcodec_find_decoder(iCodecCtx->codec_id);
+    if (iCodec == nullptr) {
+        error = "iCodec not found";
+        LOGD("error: %s\n",error);
+        return;
+    }
+
+    //7.打开解码器
+    if (avcodec_open2(iCodecCtx, iCodec, nullptr) < 0) {
+        error = "Could not open codec,";
+        LOGD("error: %s\n",error);
+        return;
+    }
+
+    //8解码
+    img_convert_ctx = sws_getContext(iCodecCtx->width, iCodecCtx->height, iCodecCtx->pix_fmt,
+                                     iCodecCtx->width, iCodecCtx->height, AV_PIX_FMT_RGBA,
+                                     SWS_BICUBIC,
+                                     nullptr, nullptr, nullptr);
+    numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGBA, iCodecCtx->width, iCodecCtx->height, 1);
+    out_buffer = (uint8_t *) av_malloc(numBytes * sizeof(uint8_t));
+
+    av_image_fill_arrays(oFrameRGB->data, oFrameRGB->linesize, out_buffer, AV_PIX_FMT_RGBA,
+                         iCodecCtx->width, iCodecCtx->height, 1);
+    int y_size = iCodecCtx->width * iCodecCtx->height;
+    packet = (AVPacket *) malloc(sizeof(AVPacket));
+    av_new_packet(packet, y_size);
+
+    if (0 > ANativeWindow_setBuffersGeometry(nativeWindow, iCodecCtx->width, iCodecCtx->height,
+                                             WINDOW_FORMAT_RGBA_8888)) {
+        ANativeWindow_release(nativeWindow);
+        return;
+    }
+    int num = 0;
+    while (1) {
+        num++;
+        if (av_read_frame(iFormatCtx, packet) < 0) {
+            break;
+        }
+        if (packet->stream_index == videoStream) {
+            ret = avcodec_send_packet(iCodecCtx, packet);
+            if (ret < 0) {
+                break;
+            }
+            got_picture = avcodec_receive_frame(iCodecCtx, iFrame);
+            if (!got_picture) {
+                sws_scale(img_convert_ctx, (uint8_t const *const *) iFrame->data, iFrame->linesize,
+                          0, iCodecCtx->height, oFrameRGB->data, oFrameRGB->linesize);
+                LOGD("decode is %s\n frame",num);
+//                if (ANativeWindow_lock(nativeWindow,&windowBuffer, nullptr)<0){
+//                    LOGD("cannot lock window");
+//                } else{
+//                    uint8_t *dst = (uint8_t *)windowBuffer.bits;
+//                    for(int h =0;h<iCodecCtx->height;h++){
+//                        memcpy(dst+h*windowBuffer.stride*4,out_buffer+h+oFrameRGB->linesize[0],oFrameRGB->linesize[0]);
+//                    }
+//                    ANativeWindow_unlockAndPost(nativeWindow);
+//                }
+
+
+            }
+        }
+        av_packet_unref(packet);
+    }
+    av_free(out_buffer);
+    av_free(iFrame);
+    av_free(oFrameRGB);
+    avcodec_close(iCodecCtx);
+    avformat_close_input(&iFormatCtx);
+}
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_qchenmo_ff001_MainActivity_log(JNIEnv *env, jobject thiz) {
+    // TODO: implement log()
+    const char *error = "I am a log test";
+    LOGD("this is %s\n",error);
 }
--- ff001/app/src/main/java/com/qchenmo/ff001/MainActivity.java
+++ ff001/app/src/main/java/com/qchenmo/ff001/MainActivity.java
@@ -3,6 +3,11 @@ package com.qchenmo.ff001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,21 +15,65 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("avcodec");
+        System.loadLibrary("avformat");
+        System.loadLibrary("swscale");
+        System.loadLibrary("avutil");
     }
 
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    Button btn_play;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hello();
+            }
+        });
+        //textView.setText(hello());
+//        surfaceHolder = surfaceView.getHolder();
+//        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+//            @Override
+//            public void surfaceCreated(SurfaceHolder holder) {
+//                String inputUrl = "rtsp://192.168.1.103/11";
+//                play(inputUrl,surfaceHolder.getSurface());
+//            }
+//
+//            @Override
+//            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+//
+//            }
+//
+//            @Override
+//            public void surfaceDestroyed(SurfaceHolder holder) {
+//
+//            }
+//        });
+
+
+
     }
+    private void initView(){
+        btn_play = findViewById(R.id.btn_play);
+        textView = findViewById(R.id.tv_show);
+        surfaceView = findViewById(R.id.surface);
+    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String log();
+    public native String hello();
+
+    public native void play(String url, Surface surface);
+
 }
